1c5a22a3b5a5ff4d0c04e6c0baac9ac98ac1c4a8
vagrant <vagrant@homestead>
2018-05-07 19:32:18 +0000

    add goods

--- app/Libraries/MeiTuanOpenApi/Api/OrderService.php
+++ app/Libraries/MeiTuanOpenApi/Api/OrderService.php
@@ -16,11 +16,11 @@ class OrderService extends RpcService
         $result = json_decode($this->client->call("/order/confirm", $params, 'GET'), true);
         if ($result && $result['data'] == 'ok')
         {
-            $log = new OrderLog();
-            $log->order_id = $order_id;
-            $log->message = '确认订单成功';
-            $log->operator = 'system';
-            $log->save();
+//            $log = new OrderLog();
+//            $log->order_id = $order_id;
+//            $log->message = '确认订单成功';
+//            $log->operator = 'system';
+//            $log->save();
             return true;
         }
         return isset($result['error']['msg'])?trim($result['error']['msg']):'请求确认接口错误';
@@ -36,11 +36,11 @@ class OrderService extends RpcService
         $result = json_decode($this->client->call("/order/cancel", $params, 'GET'), true);
         if ($result && $result['data'] == 'ok')
         {
-            $log = new OrderLog();
-            $log->order_id = $order_id;
-            $log->message = '取消订单成功';
-            $log->operator = 'system';
-            $log->save();
+//            $log = new OrderLog();
+//            $log->order_id = $order_id;
+//            $log->message = '取消订单成功';
+//            $log->operator = 'system';
+//            $log->save();
             return true;
         }
         return isset($result['error']['msg'])?$result['error']['msg']:'请求取消接口错误';
@@ -54,11 +54,11 @@ class OrderService extends RpcService
         $result = json_decode($this->client->call("/order/delivering", $params, 'GET'), true);
         if ($result && $result['data'] == 'ok')
         {
-            $log = new OrderLog();
-            $log->order_id = $order_id;
-            $log->message = '配送订单成功';
-            $log->operator = 'system';
-            $log->save();
+//            $log = new OrderLog();
+//            $log->order_id = $order_id;
+//            $log->message = '配送订单成功';
+//            $log->operator = 'system';
+//            $log->save();
             return true;
         }
         return isset($result['error']['msg'])?$result['error']['msg']:'请求配送接口错误';
