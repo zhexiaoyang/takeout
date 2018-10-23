6c477691b6e9c16cec03c802f4bd4d980638ee8c
vagrant <vagrant@homestead>
2018-10-23 03:15:48 +0000

    index

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -40,15 +40,15 @@ class AppServiceProvider extends ServiceProvider
 //                $refunds = Order::allowShops()->select(['id', 'order_id'])->where('apply_refund', '=', 1)->where('refund_at', '>', date('Y-m-d H:i:s', (time() - 1800)))->orderBy('id', 'desc')->get()->toArray();
 
                 $_print_orders = Cache::remember('is_print', 2, function () {
-                    return Order::allowShops()->with('shop')->select(['id', 'order_id', 'shop_id'])->where('is_print', 0)->where('status', '<', 20)->orderBy('id', 'desc')->get()->toArray();
+                    return Order::with('shop')->select(['id', 'order_id', 'shop_id'])->where('is_print', 0)->where('status', '<', 20)->orderBy('id', 'desc')->get()->toArray();
                 });
 
                 $_apply_cancels = Cache::remember('apply_cancel', 2, function () {
-                    return Order::allowShops()->with('shop')->select(['id', 'order_id', 'shop_id'])->where('apply_cancel', '=', 1)->where('cancel_at', '>', date('Y-m-d H:i:s', (time() - 1800)))->orderBy('id', 'desc')->get()->toArray();
+                    return Order::with('shop')->select(['id', 'order_id', 'shop_id'])->where('apply_cancel', '=', 1)->where('cancel_at', '>', date('Y-m-d H:i:s', (time() - 1800)))->orderBy('id', 'desc')->get()->toArray();
                 });
 
                 $_refunds = Cache::remember('apply_refund', 2, function () {
-                    return Order::allowShops()->with('shop')->select(['id', 'order_id', 'shop_id'])->where('apply_refund', '=', 1)->where('refund_at', '>', date('Y-m-d H:i:s', (time() - 1800)))->orderBy('id', 'desc')->get()->toArray();
+                    return Order::with('shop')->select(['id', 'order_id', 'shop_id'])->where('apply_refund', '=', 1)->where('refund_at', '>', date('Y-m-d H:i:s', (time() - 1800)))->orderBy('id', 'desc')->get()->toArray();
                 });
 
                 $user_yids = User::find(Auth::id())->shopIds();
