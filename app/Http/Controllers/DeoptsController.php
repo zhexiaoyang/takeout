ae4142d81ee303e6a943170fb16691e88275f892
vagrant <vagrant@homestead>
2018-11-13 06:01:40 +0000

    标品库

--- app/Http/Controllers/DeoptsController.php
+++ app/Http/Controllers/DeoptsController.php
@@ -2,10 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use App\Models\Deopt;
+use App\Models\Good;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\DeoptRequest;
+use MeiTuanOpenApi\Api\CategoryService;
+use MeiTuanOpenApi\Api\GoodsService;
+use MeiTuanOpenApi\Config\Config;
 
 class DeoptsController extends Controller
 {
@@ -39,7 +44,10 @@ class DeoptsController extends Controller
 
 	public function store(DeoptRequest $request, Deopt $deopt)
 	{
-        $deopt->fill($request->all());
+        $shop_id = 1;
+        $data = $request->all();
+
+        $deopt->fill($data);
         if (!$deopt->unit)
         {
             $deopt->unit = '';
@@ -60,7 +68,38 @@ class DeoptsController extends Controller
         {
             $deopt->common_name = '';
         }
-        $deopt->save();
+        $status = $deopt->save();
+
+        if ($status)
+        {
+            $category = Category::where(['name' => $request->category, 'shop_id' => $shop_id])->first();
+            if (!$category)
+            {
+                $category = Category::create(['name' => $request->category, 'shop_id' => $shop_id, 'sort' => 100]);
+                $server = New CategoryService(New Config(env('MT_APPID'),env('MT_SECRET')));
+                $category_create_status = $server->create($category);
+                if (!$category_create_status)
+                {
+                    $category->delete();
+                }
+            }
+            if ($category->meituan_id)
+            {
+                $data['category_id'] = $category->id;
+                $data['price'] = 66;
+                $data['shop_id'] = $shop_id;
+                $data['deopt_id'] = $deopt->id;
+                $good = Good::create($data);
+                $server = New GoodsService(New Config(env('MT_APPID'),env('MT_SECRET')));
+                $good_create_status = $server->create($good);
+                if (!$good_create_status)
+                {
+                    $good->delete();
+                    $deopt->delete();
+                    return redirect()->back()->with('message', '创建失败');
+                }
+            }
+        }
 		return redirect()->route('deopts.show', $deopt->id)->with('message', '创建成功');
 	}
 
